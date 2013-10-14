47fe3c9d072639270d20c07646f906825ffe8840
Paul English <paul@onfrst.com>
2013-10-14 13:21:48 -0600

    Update smtp port

--- recommender/settings.py
+++ recommender/settings.py
@@ -213,7 +213,7 @@ if HEROKU_PRODUCTION:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
     EMAIL_HOST = 'smtp.gmail.com'
-    EMAIL_PORT = 465
+    EMAIL_PORT = 587
     EMAIL_HOST_USER = 'paulnglsh@gmail.com'
     EMAIL_HOST_PASSWORD = 'rofl'
     EMAIL_USE_TLS = True
