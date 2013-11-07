73dab9ff09a7795b4771d6e61af02f0a702a87a9
Paul English <paul@onfrst.com>
2013-11-07 12:52:32 -0700

    Add Angular.js and basic app module

---
+++ recommender/static/js/app.js
@@ -0,0 +1,11 @@
+window.app = angular.module('app', []);
+
+app.config(function() {
+});
+
+app.run(function() {
+    return console.log('Array(16).join("lol" - 2) + " Batman!";');
+});
+
+app.controller('AppCtrl', function($scope, $location) {
+});
