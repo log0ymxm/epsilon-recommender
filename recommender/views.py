ed8d669ea7694e28681ec09599fa7b4f56f1eaba
Paul English <paul@onfrst.com>
2013-11-06 22:07:46 -0700

    Use real images for all templated video games

--- recommender/views.py
+++ recommender/views.py
@@ -7,10 +7,9 @@ from recommender.models import VideoGame
 def home(request):
     title = "Home"
 
-    # SELECT * FROM videio_game WHERE name is not null LIMIT 5
     video_games = VideoGame.objects.filter(~Q(name='') &
-                                           ~Q(description=''))[:8]
-    print '---', video_games
+                                            ~Q(description='') &
+                                            Q(ign_image__isnull=False))[:8]
 
     return render_to_response('home.html',
                               locals(),
@@ -19,18 +18,21 @@ def home(request):
 def recommendations(request):
     title = "Recommendations"
 
-
     video_games = VideoGame.objects.filter(~Q(name='') &
-                                           ~Q(description=''))[:8]
-   
+                                            ~Q(description='') &
+                                            Q(ign_image__isnull=False))[:8]
+
     return render_to_response('recommendations.html',
                               locals(),
                               context_instance=RequestContext(request))
 
 def search_and_rate(request):
     title = "Search and Rate"
+
     video_games = VideoGame.objects.filter(~Q(name='') &
-                                           ~Q(description=''))[:5]
+                                            ~Q(description='') &
+                                            Q(ign_image__isnull=False))[:5]
+
     return render_to_response('search_and_rate.html',
                               locals(),
                               context_instance=RequestContext(request))
