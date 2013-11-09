2cb0523813117c07c8fbb22ec99e481ac6519656
Paul English <paul@onfrst.com>
2013-11-08 22:33:59 -0700

    Working on saving ratings
    + Begin work on saving rating changes
    + Include angular.js comments
    + Create factory for interacting with api (the api doesn't exist yet)

--- recommender/static/js/app.js
+++ recommender/static/js/app.js
@@ -60,10 +60,35 @@ app.directive('eRating', function() {
     };
 });
 
+app.factory("api", function($http, $resource, settings) {
+    delete $http.defaults.headers.common['X-Requested-With'];
+    var api = $resource("/api/v1/:type/:id",
+                        {
+                            type: '@type',
+                            api_key: function() {localStorage.getItem('api_key');},
+                            id: '@id'
+                        },
+                        {
+                            create: {method: 'POST', headers: {'Content-Type': 'application/json'}},
+                            update: {method: 'PUT', headers: {'Content-Type': 'application/json'}},
+                            delete: {method: 'DELETE', headers: {'Content-Type': 'application/json'}}
+                        });
+
+    return api;
+});
+
+// runs before startup, doesn't have a fully bootstrapped environment
 app.config(function() {});
 
+// same as jQuery(window).ready(function(){});
+// or $(window).ready(function(){});
+// or $(function(){});
 app.run(function() {
     return console.log('Array(16).join("lol" - 2) + " Batman!";');
 });
 
-app.controller('AppCtrl', function($scope, $location) {});
+app.controller('AppCtrl', function($scope, $location) {
+    $scope.saveVotes = function() {
+        // TODO
+    };
+});
