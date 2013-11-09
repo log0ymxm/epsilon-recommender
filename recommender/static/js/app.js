24675d1f3193f3e0dccaaaf6dd78b5573783297b
Paul English <paul@onfrst.com>
2013-11-08 21:17:50 -0700

    Currently unused functions

--- recommender/static/js/app.js
+++ recommender/static/js/app.js
@@ -60,12 +60,10 @@ app.directive('eRating', function() {
     };
 });
 
-app.config(function() {
-});
+app.config(function() {});
 
 app.run(function() {
     return console.log('Array(16).join("lol" - 2) + " Batman!";');
 });
 
-app.controller('AppCtrl', function($scope, $location) {
-});
+app.controller('AppCtrl', function($scope, $location) {});
