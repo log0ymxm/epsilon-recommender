7df45f09551b5ecfbe37b24e30134b9f5d659c22
Paul English <paul@onfrst.com>
2013-11-11 22:42:28 -0700

    Log generated SQL queries to the console

--- recommender/settings.py
+++ recommender/settings.py
@@ -181,6 +181,10 @@ LOGGING = {
             'level': 'ERROR',
             'propagate': True,
         },
+        'django.db.backends': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+        },
     }
 }
 
